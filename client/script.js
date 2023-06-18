79a0b03583f0f4523adb38f7e1ff248c242bedf8
Sahil <sahillal25@outlook.de>
2023-06-18 22:27:57 +0200

    code

--- client/script.js
+++ client/script.js
@@ -168,7 +168,7 @@ form.addEventListener('keyup', (e)=>{
 
 const click = function(){
   if(pCount>= 6){
-    alert("_aa5qs");
+    alert("433717");
   }
   else{
     alert("Mindestens 6 prompts!");
