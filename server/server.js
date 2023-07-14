b283dd889b4a91e549544121a35741404b0ac34d
Sahil <sahillal25@outlook.de>
2023-07-14 11:29:50 +0200

    Kann sich nun Prompts merken

--- server/server.js
+++ server/server.js
@@ -2,34 +2,21 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
-/*import * as sqlite from 'sqlite3';
-const sqlite3 = sqlite;*/
+
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-//const sqlite3 = require('sqlite3').verbose();
+
 var pg = require('pg');
-//var conString ="postgres://jjcvxmos:5D9o-SZWVWJRb5a8VPaWojzwHJygUjQz@tyke.db.elephantsql.com/jjcvxmos";
 var conString = process.env.URL;
 
 var name = "";
 var task = "";
 
 dotenv.config();
-let user =[] ;
-let bot =[];
-function getMessage(){
-    var arrN =[];
-    let a=0,b=0;
-    for(let i = 0;a<user.length;i+=2 ){
-        arrN[i]=(JSON.parse(user[a++]))
-    }
-    for(let i = 1;b<bot.length;i+=2 ){
-        arrN[i]=(JSON.parse(bot[b++]))
-    }
-    return arrN;
-}
+
+
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
 
@@ -45,16 +32,15 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/', async (req, res) => {
+
+
+    name = req.body.na;
+    task = req.body.ta;
+    const nachrichten = req.body.h;
+
+    /// Antwort von chatGPT
+    let completion;
     try {
-        //const prompt = await req.body.prompt.json();
-        //const prompt = JSON.parse(req.body.prompt);
-        user= JSON.parse(req.body.u);
-        console.log(user)
-        if(user.length >1){
-         bot = JSON.parse(req.body.b);
-        }
-        name = req.body.na;
-        task = req.body.ta
         /*const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
@@ -65,16 +51,29 @@ app.post('/', async (req, res) => {
             presence_penalty: 0,
         
         });*/
-        let b = getMessage();
-        const completion = await openai.createChatCompletion({
+
+        // Anfrage an chatGPT senden
+        completion = await openai.createChatCompletion({
             model: 'gpt-3.5-turbo',
-            messages: [b],
-        })
-        
+            messages: nachrichten
+        });
+        //console.log(completion);
+
+        // Antwort an Client senden
         res.status(200).send({
             //bot: response.data.choices[0].text
             bot: (completion.data.choices[0].message),
-        })
+        });
+
+    } catch (error) {
+        console.log("Anfragen Fehler");
+        console.log(error);
+        res.status(500).send({ error });
+        return; // todo DB Part unabhängig von Antwort machen
+    }
+
+    try {
+        // DB verbinden
         var client = new pg.Client(conString);
         client.connect(function (err) {
             if (err) {
@@ -83,23 +82,24 @@ app.post('/', async (req, res) => {
         });
         console.log("Verbunden");
 
+        // Nachricht in DB speichern
         const obj = completion.data.choices[0].message;
-        const parsedData   = obj.content.trim();
+        const parsedData = obj.content.trim();
         let sql = "INSERT INTO Prompt VALUES('" + req.body.prompt + "','" + parsedData + "','" + name + "','" + task + "');";
         console.log(sql);
-        client.query(sql, function (err, result) {
+        /*client.query(sql, function (err, result) {
             if (err) {
                 return console.error('error running query', err);
             }
             //console.log(result.rows[0].theTime);
             // >> output: 2018-08-23T14:02:57.117Z
             client.end();
-        });
-    
+        });*/
+
     } catch (error) {
+        console.log("DB Fehler");
         console.log(error);
-        console.log(getMessage())
-        res.status(500).send({error});
+        //res.status(500).send({ error });
     }
 
 
